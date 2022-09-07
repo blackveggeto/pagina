4e10974a24a27d4938ea284aaa6d9e568528790b
Felipe Aguilar <luis.aguilartaisiguez@ucr.ac.cr>
2022-09-06 18:34:36 -0600

    subidad de prueba

---
+++ src/components/Login.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import {Menu} from './Menu'
+
+export const Login = () => {
+
+    const [miLogin, setLoging] = useState("false");
+
+    const [datos, setDatos] = useState({
+        name: '',
+        lastName: ''
+    });
+
+    const handleChange = (event) => {
+        console.log(event.target.value)
+        setDatos({
+            ...datos,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    function initSetion(e) {
+        e.preventDefault();
+        let usuario = datos.name;
+        let password = datos.lastName;
+
+        if (usuario.length === 0 || password.length === 0) {
+            alert("complete los datos");
+        } else {
+            if (usuario === "felipe" && password === "1234") {
+                setLoging("true");
+                document.getElementById("form_login").style.display = "none";
+
+            } else {
+                setLoging("false");
+                alert("usuario o contraseña malos");
+                document.getElementById("txtusu").value = "";
+                document.getElementById("txtpas").value = "";
+                document.getElementById("txtusu").focus();
+
+            }
+        }
+
+    }
+
+    return (
+        <div className="container" style={{ background: "lightgray", marginTop: 20, padding: 20 }}>
+            <form id="form_login">
+                <div>
+                    <h1 style={{ color: "blue", textalign: "center" }}>LOGIN</h1>
+                    <label htmlFor="txtusu"><strong>Username</strong></label>
+                    <input type="text" id="txtusu" style={{ textAlign: "center" }} className="form-control"
+                        name="name"
+                        onChange={handleChange}
+
+                        required />
+                </div>
+                <div>
+                    <label htmlFor="txtpas"><strong>Password</strong></label>
+                    <input type="password" id="txtpas" style={{ textAlign: "center" }} className="form-control"
+                        name="lastName"
+                        onChange={handleChange}
+
+                        required />
+                </div><br />
+                <input type="submit" className="btn btn-primary" value="Ingresar" onClick={initSetion} />
+
+            </form>
+            {miLogin==="true" && <Menu user={datos.name}/>}
+        </div>
+    )
+}
+
---
+++ src/components/Menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export const Menu = (props) => {
+  return (
+    <div>
+        <label>  {props.user}</label>
+    </div>
+  )
+}
