fbaeafb22f7a8557c205257d59b225e484a4df20
Felipe Aguilar <luis.aguilartaisiguez@ucr.ac.cr>
2022-09-07 02:31:37 -0600

    actualizacion de app en login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import {Login} from './components/Login';
 
@@ -7,7 +6,7 @@ function App() {
     <main className="App">
       <BrowserRouter>
       <Routes>
-        <Route exact path="/" component={Login}/>
+        <Route exact path="/" element={<Login/>}/>
       </Routes>
       </BrowserRouter>
     </main>
