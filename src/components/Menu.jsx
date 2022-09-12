4c86e29bb7b7fe8b12654c788ade96db775ec13a
Felipe Aguilar <luis.aguilartaisiguez@ucr.ac.cr>
2022-09-12 00:21:34 -0600

    actualizacion aun mas css 5

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './desing/desing.css';
+import Imagen from './desing/LOVE.jpg'
 
 export const Menu = (props) => {
   return (
     <div>
-      <label className='label'> Nombre de Usuario:  {props.user}</label><br></br>
+      <label className='label'> Usuario:  {props.user}</label><br></br>
       <div className='mes'>
         <h1>Te quiero Mucho Yet</h1>
+      </div>
+      <div className='imagen'>
+      <img src={Imagen} width="700" height="700"/>
 
       </div>
 
