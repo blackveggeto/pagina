c2441bbb3aaea688576014fed2268049fc873f3a
Felipe Aguilar <luis.aguilartaisiguez@ucr.ac.cr>
2022-09-15 13:41:30 -0600

    nueva subida

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,11 +8,6 @@ export const Menu = (props) => {
     <div>
       <label className='label'> Usuario:  {props.user}</label><br></br>
       <div className='mes'>
-        <h1>Te quiero Mucho Yet</h1>
-      </div>
-      <div className='imagen'>
-      <img src={Imagen} width="700" height="700"/>
-
       </div>
 
     </div>
